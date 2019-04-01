5122a7b058ddc04d570aee8a80c05c9f9d0266e2
Ryan Birch <ryan.birch@gmail.com>
2019-04-01 04:30:18 -0400

    made progress on game logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,115 @@
-let randomNumText = document.getElementById('random-number'),
-    winsText = document.getElementById('wins'),
-    lossesText = document.getElementById('losses'),
-    totalScoreText = document.getElementById('total-score')
+$(document).ready(function() {
+
+  let $targetNumText = $('#target-number'),
+      $winsText = $('#wins'),
+      $lossesText = $('#losses'),
+      $totalScoreText = $('#total-score')
+      $alertText = $('#alert')
 
 
-// initial game state
+  // initial game state
 
-// random number is between 19 and 120
-let randomNum = Math.floor(Math.random() * 102) + 19,
-    wins = 0,
-    losses = 0,
+  // target number is between 19 and 120
+  let targetNum = Math.floor(Math.random() * 102) + 19,
+      wins = 0,
+      losses = 0,
+      totalScore = 0
+
+  // crystal values are between 1 and 12
+  let crystal_1 = Math.floor(Math.random() * 12) + 1,
+      crystal_2 = Math.floor(Math.random() * 12) + 1,
+      crystal_3 = Math.floor(Math.random() * 12) + 1,
+      crystal_4 = Math.floor(Math.random() * 12) + 1
+
+  $targetNumText.html(targetNum)
+  $winsText.html(wins)
+  $lossesText.html(losses)
+  $totalScoreText.html(totalScore)
+
+
+  // resets the game after a win or loss
+  function reset() {
+    targetNum = Math.floor(Math.random() * 102) + 19
+    $targetNumText.html(targetNum)
     totalScore = 0
-
-// crystal values are between 1 and 12
-let crystal_1 = Math.floor(Math.random() * 12) + 1,
-    crystal_2 = Math.floor(Math.random() * 12) + 1,
-    crystal_3 = Math.floor(Math.random() * 12) + 1,
+    $totalScoreText.html(totalScore)
+    crystal_1 = Math.floor(Math.random() * 12) + 1
+    crystal_2 = Math.floor(Math.random() * 12) + 1
+    crystal_3 = Math.floor(Math.random() * 12) + 1
     crystal_4 = Math.floor(Math.random() * 12) + 1
+  }
 
-randomNumText.textContent = randomNum
-winsText.textContent = wins
-lossesText.textContent = losses
-totalScoreText.textContent = totalScore 
+  // resets the alert at the beginning of a new game
+  function wipeAlert() {
+    if (totalScore === 0) $alertText.html('')
+  }
+
+  function addToTotal(crystal) {
+    totalScore += crystal
+    $totalScoreText.html(totalScore)
+  }
 
 
-$('.c1').on('click', function() {
+  $('.c1').on('click', function() {
+    wipeAlert()
 
-})
+    totalScore += crystal_1
+    $totalScoreText.html(totalScore)
+    if (totalScore === targetNum) {
+      $alertText.html('You win!')
+      reset()
+    }
+    if (totalScore > targetNum) {
+      $alertText.html('You lose :(')
+      reset()
+    }
+  })
 
-$('.c1').on('click', function() {
+  $('.c2').on('click', function() {
+    wipeAlert()
 
-})
+    totalScore += crystal_2
+    $totalScoreText.html(totalScore)
 
-$('.c1').on('click', function() {
+    if (totalScore === targetNum) {
+      $alertText.html('You win!')
+      reset()
+    }
+    if (totalScore > targetNum) {
+      $alertText.html('You lose :(')
+      reset()
+    }
+  })
 
-})
+  $('.c3').on('click', function() {
+    wipeAlert()
 
-$('.c1').on('click', function() {
+    totalScore += crystal_3
+    $totalScoreText.html(totalScore)
 
+    if (totalScore === targetNum) {
+      $alertText.html('You win!')
+      reset()
+    }
+    if (totalScore > targetNum) {
+      $alertText.html('You lose :(')
+      reset()
+    }
+  })
+
+  $('.c4').on('click', function() {
+    wipeAlert()
+
+    totalScore += crystal_4
+    $totalScoreText.html(totalScore)
+
+    if (totalScore === targetNum) {
+      $alertText.html('You win!')
+      reset()
+    }
+    if (totalScore > targetNum) {
+      $alertText.html('You lose :(')
+      reset()
+    }
+  })
 })
